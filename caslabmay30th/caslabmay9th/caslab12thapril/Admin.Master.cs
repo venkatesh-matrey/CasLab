3c81a18da25798a4ac35248ad3e2253b8690cc8b
sadhana-matreytech <52217143+sadhana-matreytech@users.noreply.github.com>
2019-07-05 12:35:32 +0530

    Update Admin.Master.cs

--- caslabmay30th/caslabmay9th/caslab12thapril/Admin.Master.cs
+++ caslabmay30th/caslabmay9th/caslab12thapril/Admin.Master.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace caslab12thapril
 {
@@ -16,6 +17,46 @@ namespace caslab12thapril
         protected void Page_Load(object sender, EventArgs e)
         {
             NewEmployeeID();
+			
+            if (!Page.IsPostBack)
+            {
+               
+                Projectid.Items.Insert(0,new System.Web.UI.WebControls.ListItem(" Search ", " "));
+                Projectid.AppendDataBoundItems = true;
+
+                String strQuery = "select Id,Name from AddProject3 ";
+                SqlConnection con = new SqlConnection(str);
+
+                SqlCommand cmd = new SqlCommand();
+                
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strQuery;
+                cmd.Connection = con;
+                try
+                {
+                    //con.Open();
+                    //Projectid.DataSource = cmd.ExecuteReader();
+                    //Projectid.DataTextField = "Name";
+                    //Projectid.DataValueField = "Id";
+                    //Projectid.DataBind();
+
+                    con.Open();
+                    Projectid.DataSource = cmd.ExecuteReader();
+                    Projectid.DataTextField = "Name";
+                    Projectid.DataValueField = "Id";
+                    Projectid.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                    con.Dispose();
+                }
+                
+            }
         }
         private void NewEmployeeID()
         {
@@ -71,10 +112,77 @@ namespace caslab12thapril
         }
 
        
-        protected void homebutton_Click(object sender, EventArgs e)
+        protected void Projectid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = this.GetData("SELECT Id, Name,ProjectName,ProjectDescription,EmpId FROM AddProject3 where Name= '" + Projectid.SelectedItem.Text + "' ");
+            TreeView1.Nodes.Clear();
+            this.PopulateTreeView(dt, 0, null);
+            
+        }
+   private DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            string str = ConfigurationManager.ConnectionStrings["MyDbCon"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(str))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+
+
+
+        private void PopulateTreeView(DataTable dtParent, int parentId, TreeNode treeNode)
         {
 
-            Response.Redirect("Dashboard.aspx");
+            foreach (DataRow row in dtParent.Rows)
+            {
+
+                TreeNode child = new TreeNode
+                {
+
+                    Text = row["Name"].ToString(),
+                    Value = row["Id"].ToString()
+
+
+                };
+
+                if (parentId == 0)
+                {
+                    TreeView1.Nodes.Add(child);
+
+                    DataTable dtChild = this.GetData("SELECT Id, Name,TaskDescription,EmpId,UserName,projectid  FROM AddnewTask2 WHERE  projectid = '" + Projectid.SelectedItem.Text + "'");
+
+                    PopulateTreeView(dtChild, int.Parse(child.Value), child);
+
+
+                }
+                else
+                {
+                    treeNode.ChildNodes.Add(child);
+
+
+
+                }
+
+
+
+
+            }
+        }
+        protected void homebutton_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("Admindashboard.aspx");
+            TreeView1.Nodes.Clear();
         }
 
         protected void Button2_Click1(object sender, EventArgs e)
